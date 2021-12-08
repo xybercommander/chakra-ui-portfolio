960e8f8c22aa52f0c681911841a041bd0c2cf725
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-12-08 19:19:59 +0530

    fixed ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import { useColorMode } from '@chakra-ui/color-mode'
 import { Box, Circle, Stack, Text } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/button';
+import { Image } from '@chakra-ui/image';
 import { useMediaQuery } from '@chakra-ui/media-query';
 import { Flex } from '@chakra-ui/react';
 import React from 'react'
@@ -21,9 +23,24 @@ function Header() {
           alignSelf="flex-start"
         >
 
-          <Box ml={isNotSmallerScreen ? "4" : "16"} alignSelf="flex-start">
-            <Text fontSize="5x1" fontWeight="semibold">Hi, I am</Text>
+          <Box ml={isNotSmallerScreen ? "0" : "16"} alignSelf="flex-start">
+            <Text fontSize="5xl" fontWeight="semibold">Hi, I am</Text>
+            <Text 
+              bgColor="blue.200" 
+              bgClip="text"
+              fontSize="7xl" 
+              fontWeight="bold"               
+            >
+              Xybercommander
+            </Text>
+            <Text bgColor={isDark ? "gray.200" : "gray.500"} bgClip="text">
+              Highly motivated self taught CS undergrad eager to learn new technologies to make
+              modern mobile and web applications. I am confident with my ability to provide
+              optimized code to make outstanding applications with the latest frameworks.
+            </Text>
+            <Button mt={8} colorScheme="blue" onClick={() => window.open("https://github.com/xybercommander")}>My Github</Button>
           </Box>
+          <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"} mb={isNotSmallerScreen ? "0" : "12"} borderRadius="full" backgroundColor="transparent" boxShadow="lg" boxSize="300px" src="https://avatars.githubusercontent.com/u/38849650?v=4" />
 
         </Flex>
       </Stack>
